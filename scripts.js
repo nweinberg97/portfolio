2607a400e5f96b83b2ed7f81a473fbdf93aedeeb
nweinberg97 <111965378+nweinberg97@users.noreply.github.com>
2024-10-27 11:43:33 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,30 @@ const hamburger = document.querySelector('.hamburger');
     hamburger.addEventListener('click', () => {
         navLinks.classList.toggle('show'); // Toggle the "show" class on click
     });
+
+console.log("scripts.js file is loaded");
+
+document.addEventListener("DOMContentLoaded", function() {
+    // EmailJS configuration
+    emailjs.init("qZCMk0yO7Rwu_EXe2");
+
+    // Handle contact form submission
+    document.querySelector("#contact form").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        var formData = {
+            from_name: document.querySelector("#name").value,
+            message: document.querySelector("#message").value,
+            reply_to: document.querySelector("#email").value,
+        };
+
+        emailjs.send("service_wrnbapd", "template_vc7abxh", formData)
+            .then(function(response) {
+                console.log("SUCCESS!", response.status, response.text);
+                alert("Your message has been sent successfully!");
+            }, function(error) {
+                console.log("FAILED...", error);
+                alert("There was an error sending your message. Please try again.");
+            });
+    });
+});
